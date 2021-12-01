464608ed793b5da34e6b9db5d54ffdf67fabd6b1
AVoit <AVoit@myemail.com>
2021-12-01 20:54:44 +0500

    ex013 сортировка от мин к макс добавил описание

--- Example013_Sort/Program.cs
+++ Example013_Sort/Program.cs
@@ -10,18 +10,18 @@ void PrintArray(int[] array) // метод вывода массива на эк
     Console.WriteLine();
 }
 
-void SelectionSort(int[] array)
+void SelectionSort(int[] array) // сортировка массива от минимального к максимальному значению
 {
     for (int i = 0; i < array.Length-1; i++)
     {
-        int minPosition = i;
+        int minPosition = i;// индекс минимального значения
         for (int j = i + 1; j < array.Length; j++)
         {
-            if (array[j] < array[minPosition]) minPosition = j;
+            if (array[j] < array[minPosition]) minPosition = j; // если элемент массива с индексом j меньше элемента массива с индексом minPosition в minPosition записывается j
         }
-        int temp = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temp;
+        int temp = array[i]; // элемент массива с индексом i записывается во временную переменную
+        array[i] = array[minPosition];// в элемент массива с индексом i перемещается минимальный элемент массива
+        array[minPosition] = temp;// в минимальную позицию записывается элемент из временной переменной
 
     }
     }

44540402b76e2f9510e9c833805df6e44ef48f40
AVoit <AVoit@myemail.com>
2021-12-01 15:02:36 +0500

    нахождение элемента массива по index

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -8,8 +8,8 @@
 //               0   1   2   3   4   5   6   7   8
 int[] array = { 21, 42, 573, 34, 55, 68, 72, 89, 49 };
 
-int max = Max(
+int result = Max(
     Max(array[0],array[1], array[2]),
     Max(array[3],array[4], array[5]), 
     Max(array[6],array[7], array[8])); // использование метода сравнения
-Console.WriteLine(max);
+Console.WriteLine(result);
---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 12, 35, 77, 41, 15, 96, 77, 28 };
+
+int n = array.Length; // возвращает длину (количество элементов) массива
+int find = 77;
+int index = 0;
+
+while(index<n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // останавливает выполнение алгоритма при получении результата
+    }
+    index++; // == index + 1
+}

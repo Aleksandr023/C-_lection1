244120c6ba22a5d8900d7f5c6b758bda9a189519
AVoit <AVoit@myemail.com>
2021-12-01 20:42:33 +0500

    ex013 сортировка от мин к макс

--- Example012_1_TextWork/Program.cs
+++ Example012_1_TextWork/Program.cs
@@ -14,24 +14,24 @@ string text = "— Я думаю, — Сказал князь, улыбаясь,
 //             0123
 // s[3] // r
 
-string Replace(string text, char OldValue, char NewValue)
+string Replace(string text, char OldValue, char NewValue) // Метод замены знаков в тексте
 {
     string result= String.Empty;
 
-    int length = text.Length;
-    for (int i = 0; i<length; i++)
+    int length = text.Length; // определение количества символов
+    for (int i = 0; i<length; i++) // i - порядковый номер символа в тексте
     {
-        if(text[i] == OldValue) result = result + $"{NewValue}";
-        else result = result + $"{text[i]}";
+        if(text[i] == OldValue) result = result + $"{NewValue}"; // описание: если элемент текста имеет старое значение, то в результат записывается новое значение
+        else result = result + $"{text[i]}"; // в другом случае в результат идет текущий символ
     }
 
 
     return result;
 }
-string NewText = Replace(text, ' ', '|');
+string NewText = Replace(text, ' ', '|'); //вводим переменную в которой применяем метод замены значений элементов
 //Console.WriteLine(NewText);
 //Console.WriteLine();
-NewText = Replace(NewText, 'к', 'К');
+NewText = Replace(NewText, 'к', 'К'); // в переменной применяем метод еще раз
 //Console.WriteLine(NewText);
 //Console.WriteLine();
 NewText = Replace(NewText, 'С', 'с');
---
+++ Example013_Sort/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] arr = { 1, 5, 2, 9, 4, 6, 3, 1, 1 };
+
+void PrintArray(int[] array) // метод вывода массива на экран
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} "); // выводит элемент массива
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+
+    }
+    }
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
